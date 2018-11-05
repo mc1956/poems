e37fd70adee596952179a6e365fd628b2650b8e1
mc1956 <mc1956@g.rit.edu>
2018-11-04 23:43:36 -0500

    list of links

--- poems.py
+++ poems.py
@@ -3,6 +3,19 @@ from bs4 import BeautifulSoup
 html = urlopen('https://www.poemas-del-alma.com/ruben-dario.htm')
 bs = BeautifulSoup(html, "html.parser")
 
-nameList = bs.findAll('ul', {'class': 'list-poems'})
-for name in nameList:
-    print(name.get_text())
+# the links :
+
+for ultag in bs.find_all('ul', {'class': 'list-poems'}):
+     for litag in ultag.find_all('li'):
+             print(litag) #print (type(ultag.find_all('li')))
+
+# find.All outputs an "array" ,
+#so you have to loop through it to access the elements
+# In this case we have only one ul f class "list-poems"
+# so we can just select the first element like below
+
+# The titles :
+
+uls = bs.find_all('ul', {'class': 'list-poems'})[0]
+for litag in uls.find_all('li'):
+    print (litag.text)
