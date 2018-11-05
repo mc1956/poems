bf4a7b47d944a92bba7c69dfd0db70f995f7649f
mc1956 <mc1956@g.rit.edu>
2018-11-04 22:39:20 -0500

    correct url

--- poems.py
+++ poems.py
@@ -1,8 +1,8 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-html = urlopen('http://www.pythonscraping.com/pages/warandpeace.html')
-bs = BeautifulSoup(html.read(), "html.parser")
-print(bs)  # All the website
+html = urlopen('https://www.poemas-del-alma.com/ruben-dario.htm')
+bs = BeautifulSoup(html, "html.parser")
+
 nameList = bs.findAll('ul', {'class': 'list-poems'})
 for name in nameList:
     print(name.get_text())
